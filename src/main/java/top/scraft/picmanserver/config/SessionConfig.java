1d531463d3e216e0436eb8fba1acd3825e88622b
kuresaru <kuresaru@scraft.top>
2021-03-15 14:02:42 +0800

    修复关闭浏览器后退出登录的问题

--- src/main/java/top/scraft/picmanserver/config/SessionConfig.java
+++ src/main/java/top/scraft/picmanserver/config/SessionConfig.java
@@ -1,9 +1,20 @@
 package top.scraft.picmanserver.config;
 
+import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400 * 30, redisNamespace = "picman:session")
-public class SessionConfig {
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = SessionConfig.COOKIE_AGE, redisNamespace = "picman:session")
+public class SessionConfig implements DefaultCookieSerializerCustomizer {
+
+    protected static final int COOKIE_AGE = 86400 * 30;
+
+    @Override
+    public void customize(DefaultCookieSerializer serializer) {
+        serializer.setUseBase64Encoding(false);
+        serializer.setCookieMaxAge(COOKIE_AGE);
+    }
+
 }
